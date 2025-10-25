a015c51d946ea0ebd3f914e9287ce1e5204f1ad2
yuzhangoscar <yuzhang2002@gmail.com>
2025-10-26 10:20:22 +1100

    [CODE] relocated cypress.config.ts and tsconfig.json to root level

---
+++ cypress.config.ts
@@ -0,0 +1,26 @@
+// cypress.config.ts
+import { defineConfig } from 'cypress';
+import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor';
+import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
+
+export default defineConfig({
+  e2e: {
+    specPattern: 'tests/E2E/Cypress/cypress/e2e/**/*.feature',
+    supportFile: 'tests/E2E/Cypress/cypress/support/e2e.ts',
+    async setupNodeEvents(on, config) {
+      // Enable Cucumber
+      await addCucumberPreprocessorPlugin(on, config);
+
+      // ESBuild for TypeScript
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
+  },
+});
